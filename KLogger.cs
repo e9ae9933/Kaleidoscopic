6f74cead41d45a8b4fc6b4a555cee75e28c2074f
example <example@example.com>
2026-03-28 02:37:51 +0800

    Initial commit

---
+++ KLogger.cs
@@ -0,0 +1,18 @@
+﻿global using static Kaleidoscopic.KLogger;
+
+namespace Kaleidoscopic;
+
+public class KLogger {
+    public static void info(params object[] obj) {
+        KaleidoscopicPlugin.LOGGER.LogInfo(string.Join(" ", obj));
+    }
+    public static void warning(params object[] obj) {
+        KaleidoscopicPlugin.LOGGER.LogWarning(string.Join(" ", obj));
+    }
+    public static void error(params object[] obj) {
+        KaleidoscopicPlugin.LOGGER.LogError(string.Join(" ", obj));
+    }
+    public static void debug(params object[] obj) {
+        KaleidoscopicPlugin.LOGGER.LogDebug(string.Join(" ", obj));
+    }
+}
---
+++ KaleidoscopicPlugin.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using BepInEx;
+using BepInEx.Logging;
+using UnityEngine;
+
+namespace Kaleidoscopic;
+
+[BepInPlugin("org.aliceincradle.kaleidoscopic", "Kaleidoscopic", "0.1.0")]
+public class KaleidoscopicPlugin : BaseUnityPlugin {
+    internal static KaleidoscopicPlugin INSTANCE = null;
+    internal static ManualLogSource LOGGER = null;
+    internal void Awake() {
+        this.Logger.LogInfo("loading Kaleidoscopic plugin");
+        try {
+            INSTANCE = this;
+            LOGGER = this.Logger;
+            info("KLogger info finished");
+            
+        } catch (Exception ex) {
+            this.Logger.LogError(ex.ToString());
+            Application.Quit("Kaleidoscopic".GetHashCode());
+            return;
+        }
+        this.Logger.LogInfo("Kaleidoscopic plugin load complete");
+    }
+    internal void Update() {
+        
+    }
+}
